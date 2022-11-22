757ee3b5c4bec5b3a17d88b0252d03deb10c4906
Thiago <tlcrezende@gmail.com>
2022-11-22 19:18:05 -0300

    Adiciona namespace api e ordena heros

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -1,18 +1,22 @@
-class HeroesController < ApplicationController
+class Api::HeroesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_hero, only: %i[ show edit update destroy ]
 
   # GET /heroes or /heroes.json
   def index
-    @heroes = Hero.all
+    @heroes = Hero.all.organizar_por_nome
+    render json: @heroes
   end
 
   # GET /heroes/1 or /heroes/1.json
   def show
+    render json: @hero
   end
 
   # GET /heroes/new
   def new
     @hero = Hero.new
+    render json: @hero
   end
 
   # GET /heroes/1/edit
@@ -32,29 +36,20 @@ class HeroesController < ApplicationController
         format.json { render json: @hero.errors, status: :unprocessable_entity }
       end
     end
+    render json: @hero
   end
 
   # PATCH/PUT /heroes/1 or /heroes/1.json
   def update
-    respond_to do |format|
-      if @hero.update(hero_params)
-        format.html { redirect_to hero_url(@hero), notice: "Hero was successfully updated." }
-        format.json { render :show, status: :ok, location: @hero }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @hero.errors, status: :unprocessable_entity }
-      end
-    end
+
+    @hero.update(hero_params)
+    render json: @hero
   end
 
   # DELETE /heroes/1 or /heroes/1.json
   def destroy
     @hero.destroy
-
-    respond_to do |format|
-      format.html { redirect_to heroes_url, notice: "Hero was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    render json: @hero
   end
 
   private
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,5 @@
 class Hero < ApplicationRecord
+    
     validates :name, presence: true
+    scope :organizar_por_nome, -> { order(:name) }
 end
