dd6bc1c213ffd2da768432c3975b694c99eb76a4
Thiago <tlcrezende@gmail.com>
2022-11-23 20:48:29 -0300

    Valida nome unico por token

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,7 @@
 class Hero < ApplicationRecord
     
     validates :name, :token, presence: true
+    validates :name, uniqueness: {scope: :token, case_sensitive: false}
     scope :organizar_por_nome, -> { order(:name) }
     scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
     scope :by_token, -> (token) {where(token: token)}
